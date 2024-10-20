18d81e96fa9b04d6cf29144ba908408740923071
Jose Luis Garcia Mtz <garcia_martinez_2011@hotmail.com>
2024-10-20 02:42:56 -0600

    Diseño mejorado

--- catalogofrontend/src/ComponentsJS/Menu/Menu.js
+++ catalogofrontend/src/ComponentsJS/Menu/Menu.js
@@ -9,36 +9,42 @@ const Menu = function () {
   };
 
   return (
-    <div>
-      <ul className="menu">
-        <li className="link">
-          <Link className="color crud" to="/">
-            <button>CRUD</button>
-          </Link>{" "}
-        </li>
-        {localStorage.getItem("token") === null ? (
-          <div className="reglog">
-            <li className="link link1">
-              <Link className="color" to="/Login">
-                <button>Login</button>
-              </Link>
-            </li>
-            <li className="link link2">
-              <Link className="color" to="/Register">
-                <button>Register</button>
+    <div className="menuscont">
+      <div className="menu-container">
+        <p className="menup2">Menu</p>
+
+        <div className="menu2">
+          <ul className="menu">
+            <li className="link">
+              <Link className="color crud" to="/">
+                <button>CRUD</button>
               </Link>{" "}
             </li>
-          </div>
-        ) : (
-          ""
-        )}
+            {localStorage.getItem("token") === null ? (
+              <div className="reglog">
+                <li className="link link1">
+                  <Link className="color" to="/Login">
+                    <button>Login</button>
+                  </Link>
+                </li>
+                <li className="link link2">
+                  <Link className="color" to="/Register">
+                    <button>Register</button>
+                  </Link>{" "}
+                </li>
+              </div>
+            ) : (
+              ""
+            )}
 
-        <li className="link link3">
-          <Link onClick={handleCerrar} className="color" to="/Login">
-            <button> Cerrar Sesion</button>
-          </Link>{" "}
-        </li>
-      </ul>
+            <li className="link link3">
+              <Link onClick={handleCerrar} className="color" to="/Login">
+                <button> Logout</button>
+              </Link>{" "}
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
